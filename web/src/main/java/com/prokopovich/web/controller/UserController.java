0b96fe7662104ede137de2eec52174674d90aa33
SHiCK <shickn1@gmai.com>
2023-07-31 17:11:20 +0300

    initial commit

--- web/src/main/java/com/prokopovich/web/controller/UserController.java
+++ web/src/main/java/com/prokopovich/web/controller/UserController.java
@@ -3,6 +3,8 @@ package com.prokopovich.web.controller;
 import com.prokopovich.repository.entity.User;
 import com.prokopovich.service.exception.UserAlreadyExistException;
 import com.prokopovich.service.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +16,14 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private Logger logger = LogManager.getLogger(UserController.class);
+
     @PostMapping
     public ResponseEntity registration(@RequestBody User user) {
         try {
             userService.registration(user);
             String massage = "User successfully created";
-            //logger.info(massage);
+            logger.info(massage);
             return ResponseEntity.ok(massage);
         } catch (UserAlreadyExistException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
